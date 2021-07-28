e706f844e7fc2b2f4791fd3644089a9bd7f354c7
Yu-Chun Tsai <jkevinz04@gmail.com>
2021-07-28 16:19:09 +0800

    add while loop

--- age.py
+++ age.py
@@ -1,7 +1,7 @@
 driving = input('請問你有沒有開過車？： ')
-if driving != '有' and driving != '沒有':
+while driving != '有' and driving != '沒有':
 	print('只能輸入 有/沒有 喔')
-	#raise SystemExit
+	driving = input('請問你有沒有開過車？： ')
 age = input('請問你的年齡： ')
 age = int(age)
 if driving == '有':
